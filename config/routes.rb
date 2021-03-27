44b54c14f9484e4e58580368e97eea099a789bcb
Tanapol <tanapol.bu@ku.th>
2021-03-27 19:23:42 +0700

    add routes config

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  devise_for :admins
+  devise_for :users
+
+  root to: 'products#index'
+
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
+
+
+  namespace :admin do
+    root to: 'products#index'
+    resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  end
+
+end
